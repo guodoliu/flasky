cceff1dab21c904d85111c54eeb22335cf05704d
Guodong Liu <gliu@splunk.com>
2019-03-20 16:10:13 +0800

    followers

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <jellypop@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASKY_POSTS_PER_PAGE = os.environ.get('FLASKY_POSTS_PER_PAGE')
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
